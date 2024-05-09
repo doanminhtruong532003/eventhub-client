d84bd943ade291e0cc6832ca37ac8bbdccda1402
roots.53 <153526732+doanminhtruong532003@users.noreply.github.com>
2024-05-09 16:33:14 +0700

    Add global style and several UI components

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import ButtonComponent from './ButtonComponent';
+import CircleComponent from './CircleComponent';
+import ContainerComponent from './ContainerComponent';
+import InputComponent from './InputComponent';
+import RowComponent from './RowComponent';
+import SectionComponent from './SectionComponent';
+import SpaceComponent from './SpaceComponent';
+import TextComponent from './TextComponent';
+
+export {
+  SpaceComponent,
+  TextComponent,
+  ButtonComponent,
+  InputComponent,
+  SectionComponent,
+  ContainerComponent,
+  RowComponent,
+  CircleComponent,
+};
